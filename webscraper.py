5aed421da22d3442f31cf41fe1bf58c0d3c9b792
Marcus Tanner <kritiosboy@gmail.com>
2017-05-04 10:01:16 -0600

    refactored into class, added pdf scraping functionality

--- webscraper.py
+++ webscraper.py
@@ -6,7 +6,10 @@ import re
 import requests
 import io
 from lxml import html
-import PyPDF2 as pdf
+from pdfminer.pdfinterp import PDFResourceManager, PDFPageInterpreter
+from pdfminer.pdfpage import PDFPage
+from pdfminer.converter import TextConverter
+from pdfminer.layout import LAParams
 import pandas as pd
 import numpy as np
 
@@ -14,110 +17,165 @@ USERNAME = "****"
 PASSWORD = "****"
 
 BASE_URL = "https://****.com"
-LOGIN_PAGE = BASE_URL + "/login"
-LOGIN_URL = BASE_URL + "/user_sessions"
+LOGIN_URL = BASE_URL + "/login"
+LOGIN_POST = BASE_URL + "/user_sessions"
+
+class AuthenticatedPDFScraper():
+    """ Used to retrieve and parse information from pdfs on authenticated websites. """
+
+    def __init__(self, username, password, login_url, login_post):
+        self.username = username
+        self.password = password
+        self.login_url = login_url
+        self.login_post = login_post
+        self.session = self.start_session()
+        self.pdf_text = ""
+
+    def start_session(self):
+        """returns a new authenticated session"""
+        session = requests.session()
+
+        # Get login csrf token
+        result = session.get(self.login_url)
+        tree = html.fromstring(result.text)
+        authenticity_token = list(
+            set(tree.xpath("//meta[@name='csrf-token']/@content")))[0]
+
+        # Create payload
+        payload = {
+            "user_session[email]": self.username,
+            "user_session[password]": self.password,
+            "authenticity_token": authenticity_token
+        }
+
+        # Perform login
+        result = session.post(
+            self.login_post, data=payload, headers=dict(referer=self.login_post))
+        return session
+
+    def get_pdf_text(self, pdf_url):
+        """ Retrieve PDF from url and convert it to a string, store in object and return. """
+        pdf_download = self.session.get(pdf_url)
+        pdf = io.BytesIO(pdf_download.content)  #convert to bytes
+
+        # PDFMiner boilerplate
+        rsrcmgr = PDFResourceManager()
+        sio = io.StringIO()
+        codec = 'utf-8'
+        laparams = LAParams()
+        device = TextConverter(rsrcmgr, sio, codec=codec, laparams=laparams)
+        interpreter = PDFPageInterpreter(rsrcmgr, device)
+
+        # extract text
+        for page in PDFPage.get_pages(pdf):
+            interpreter.process_page(page)
+
+        # Get text from StringIO
+        self.pdf_text = sio.getvalue()
+        return self.pdf_text
+
+    def get_tag_value(self, tag_name, text):
+        """return a json value from it's property name (used for broken json)"""
+        tag_name = "\"" + tag_name + "\":"
+        tag_index = text.find(tag_name)
+        comma_index = text.find(',', tag_index)
+        tag_substring = text[tag_index:comma_index]
+        tag_value = tag_substring[len(tag_name):len(tag_substring)]
+        return tag_value
 
 
-def get_tag_value(tag_name, text):
-    """return a json value from it's property name (used for broken json)"""
-    tag_name = "\"" + tag_name + "\":"
-    tag_index = text.find(tag_name)
-    comma_index = text.find(',', tag_index)
-    tag_substring = text[tag_index:comma_index]
-    tag_value = tag_substring[len(tag_name):len(tag_substring)]
-    return tag_value
+    def get_post_element_value(self, element_name, text, start_index, must_contain):
+        """return the text following a certain html element"""
+        element_index = text.find(element_name, start_index)
+        if element_index == -1:
+            return "NOTFOUND"
+
+        bracket_index = text.find('<', element_index + len(element_name))
+        element_substring = text[element_index:bracket_index]
+        element = element_substring[len(element_name):len(element_substring)]
+
+        if element.find(must_contain) != -1:
+            return element
+        else:
+            return self.get_post_element_value(element_name, text, bracket_index, must_contain)
 
 
-def get_post_element_value(element_name, text, start_index, must_contain):
-    """return the text following a certain html element"""
-    element_index = text.find(element_name, start_index)
-    if element_index == -1:
-        return "NOTFOUND"
+    def get_js_variable(self, var_name, text):
+        """return a javascript variable from it's name"""
+        var_index = text.find(var_name)
+        equal_index = text.find('=', var_index)
+        semicolon_index = text.find(';', var_index)
+        var_value = text[equal_index + 1:semicolon_index]
+        var_value = var_value.replace('"', '')  #remove quotation marks
+        return var_value
 
-    bracket_index = text.find('<', element_index + len(element_name))
-    element_substring = text[element_index:bracket_index]
-    element = element_substring[len(element_name):len(element_substring)]
-
-    if element.find(must_contain) != -1:
-        return element
-    else:
-        return get_post_element_value(element_name, text, bracket_index, must_contain)
-
-
-def get_js_variable(var_name, text):
-    """return a javascript variable from it's name"""
-    var_index = text.find(var_name)
-    equal_index = text.find('=', var_index)
-    semicolon_index = text.find(';', var_index)
-    var_value = text[equal_index + 1:semicolon_index]
-    var_value = var_value.replace('"', '')  #remove quotation marks
-    return var_value
+    def get_pdf_value(self, value_name, pdf_text):
+        """ retrieve a value from a pdf file. """
+        pdf_text = pdf_text.replace("\n\n", "\n")
+        start_idx = pdf_text.find(value_name)
+        if start_idx == -1:
+            return ""
+        sep_char = "\n"
+        end_idx = pdf_text.find(sep_char, start_idx + len(value_name) + 1)
+        value = pdf_text[start_idx:end_idx]
+        value = value.replace(value_name, "")
+        value = value.strip()
+        return value
 
 
-def start_session():
-    """returns a new authenticated session"""
-    session = requests.session()
-
-    # Get login csrf token
-    result = session.get(LOGIN_PAGE)
-    tree = html.fromstring(result.text)
-    authenticity_token = list(
-        set(tree.xpath("//meta[@name='csrf-token']/@content")))[0]
-
-    # Create payload
-    payload = {
-        "user_session[email]": USERNAME,
-        "user_session[password]": PASSWORD,
-        "authenticity_token": authenticity_token
-    }
-
-    # Perform login
-    result = session.post(
-        LOGIN_URL, data=payload, headers=dict(referer=LOGIN_URL))
-    return session
 
 
 def main():
 
+    # get command line arguments
     if len(sys.argv) < 3:
         print("Usage: webscraper.py input_file.csv output_file.csv")
         exit()
 
     input_file = sys.argv[1]
     output_file = sys.argv[2]
+
+    # read in csv
     data = pd.read_csv(input_file)
 
-    session = start_session()
+    # create scraper
+    scraper = AuthenticatedPDFScraper(USERNAME, PASSWORD, LOGIN_URL, LOGIN_POST)
 
-    col_idx = data.columns.get_loc("COLUMN NAME")
+    # perform scrape for each row in csv
+    column_name = "Proposal URL"
+    prop_url_idx = data.columns.get_loc(column_name)
     for row in data.values:
         try:
-            company_id = row[col_idx]
-            search_url = BASE_URL + "/app?search=%s&status=accepted" % company_id
-            print(company_id, end=' ')
+            # get pdf text
+            html_pdf_url = row[prop_url_idx]
+            pdf_url = "http://proposals.grow.com/pdf_generation/5620/" \
+                + html_pdf_url[html_pdf_url.find(".com/") + 5:len(html_pdf_url)] + "/download"
+            pdf_text = scraper.get_pdf_text(pdf_url)
 
-            # Scrape search_url
-            result = session.get(search_url)
-            result_htmltree = html.fromstring(result.text)
-            result_text = result.text
+            # parse pdf text
+            grand_total = scraper.get_pdf_value("Grand\nTotal:", pdf_text)
+            print("grand total: " + grand_total)
+            discount = scraper.get_pdf_value("Discount", pdf_text)
+            print("discount: " + discount)
+            term = scraper.get_pdf_value("Term:", pdf_text)
+            term_length = term[0:term.find(" ")]
+            print("term length: " + term_length)
+            auto_renew = False
+            if term.find("auto-renew"):
+                print("Auto Renew")
+                auto_renew = True
 
-            # extract proposal_id
-            prop_id = get_tag_value("proposal_id", result_text)
-            print(prop_id)
+            basic = False
+            if pdf_text.find("Basic") != -1:
+                print("Basic")
+                basic = True
 
-            # download pdf
-            prop_html_url = BASE_URL + "/app/proposals/" + prop_id
-            prop_html = session.get(prop_html_url)
-            prop_pdf_id = get_js_variable("base_proposal_path", prop_html.text)
-            print("pdf_id: " + prop_pdf_id)
-            prop_pdf_url = BASE_URL + "/pdf_generation/5620/" + prop_pdf_id + "/download"
-            prop_pdf = session.get(prop_pdf_url)
-            prop_pdf_bytes = io.BytesIO(prop_pdf.content)
-            #prop_pdf = slate.PDF(prop_pdf)
-            #print(prop_pdf.content)
-            pdf_reader = pdf.PdfFileReader(prop_pdf_bytes)
-            page_content = pdf_reader.getPage(6).extractText()
-            print(page_content)
+            trial = pdf_text.find("Trial\nPeriod:", pdf_text)
+            print(trial)
+            if trial != "":
+                trial_period = trial[0:trial.find(" ")]
+                print("trial period: " + trial_period)
+            exit()
 
         except Exception as ex:
             print(ex)
@@ -125,4 +183,3 @@ def main():
 
 if __name__ == '__main__':
     main()
-
